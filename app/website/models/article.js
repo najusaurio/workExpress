1b597aaeeac42920355eb7a7762fd0c6ab774ba4
Diego Alonso Uribe Gamez <diego.uribe.gamez@gmail.com>
2014-11-01 23:37:25 -0500

    [update] change schema in model to model

--- app/website/models/article.js
+++ app/website/models/article.js
@@ -1,34 +1,31 @@
 // dependencies
-var mongoose = require('mongoose'),
-    Schema   = mongoose.Schema;
-// shema
-var articleSchema = new mongoose.Schema({
-    active      : { type: Boolean, default: false },
-    title       : { type: String, required: true},
-    slug        : { type: String, required: true},
-    content     : String,
-    description : String,
-    imageURL    : String,
-    tags        : String,
-    published   : Date,
-    created     : { type: Date, 'default': Date.now },
-    author      : { type: Schema.ObjectId, ref:'User' },
-    hits        : Number,
-    record      : { type: Schema.ObjectId, ref:'ArticleChange' },
-});
-// shema validation
-articleSchema.path('title').validate(function(val){
-    return val.length > 70;
-});
-// shema validation
-articleSchema.path('description').validate(function(val){
-    return val.length > 160;
-});
-// shema validation
-articleSchema.path('tags').validate(function(val){
-    return val.length > 300;
-});
+var modelArticle = require('./schema/article');
+var mongoose = require('mongoose');
 // module
-Article = mongoose.model('Article', articleSchema);
+var Article = function(config){
+    config     = config || {};
+    this.model = modelArticle;
+};
+// save data
+Article.prototype.save = function(data,callback){
+    this.model.findOneAndUpdate({
+        title:data.title,
+        slug:data.slug
+    },data,{upsert:true}).exec(function(err,doc){
+        callback(doc);
+    });
+};
+// save data
+Article.prototype.remove = function(data,callback){
+    this.model.remove({slug:data.slug}).exec(function(err){
+        callback();
+    });
+};
+// get data
+Article.prototype.get = function(query,callback){
+    this.model.find(query).sort('-date').exec(function(err,docs){
+        callback(docs);
+    });
+};
 // export module
 module.exports = Article;
