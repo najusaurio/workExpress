262d2e756a8bfc74fb850daf7fa5ff93acd7c92f
Diego Alonso Uribe Gamez <diego.uribe.gamez@gmail.com>
2014-11-01 23:37:39 -0500

    [add] schema article

---
+++ app/website/models/schema/article.js
@@ -0,0 +1,34 @@
+// dependencies
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema;
+// shema
+var articleSchema = new mongoose.Schema({
+    status      : { type: Boolean, default: false },
+    title       : { type: String, required: true},
+    slug        : { type: String, required: true},
+    content     : String,
+    description : String,
+    imageurl    : String,
+    tags        : String,
+    published   : Date,
+    created     : { type: Date, 'default': Date.now },
+    author      : { type: Schema.ObjectId, ref:'User' },
+    hits        : Number,
+    record      : { type: Schema.ObjectId, ref:'ArticleChange' }
+});
+// shema validation
+articleSchema.path('title').validate(function(val){
+    return val.length > 70;
+});
+// shema validation
+articleSchema.path('description').validate(function(val){
+    return val.length > 160;
+});
+// shema validation
+articleSchema.path('tags').validate(function(val){
+    return val.length > 300;
+});
+// module
+Article = mongoose.model('Article', articleSchema);
+// export module
+module.exports = Article;
