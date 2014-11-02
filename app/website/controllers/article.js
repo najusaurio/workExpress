1a24a642ea9b4c32db01deebb199fe031a1e0d9d
Diego Alonso Uribe Gamez <diego.uribe.gamez@gmail.com>
2014-11-01 23:38:15 -0500

    [update] article controller

--- app/website/controllers/article.js
+++ app/website/controllers/article.js
@@ -1,33 +1,59 @@
+// dependencies
+var ArticleView  = require('../views/article'),
+    ArticleModel = require('../models/article');
 // module
 var Article = function(conf){
     this.conf     = conf || {};
+    this.view     = new ArticleView();
+    this.model    = new ArticleModel();
     this.response = function(){
         this[this.conf.resource](this.conf.req,this.conf.res,this.conf.next);
     };
 };
 // post save
 Article.prototype.post_save = function(req, res, next){
-    res.send('Hello from Worker');
+    this.model.save(req.body,function(doc){
+        res.redirect('/article/see/'+doc.slug);
+    });
 };
 // post remove
 Article.prototype.post_remove = function(req, res, next){
-    res.send('Hello from Worker');
+    this.model.remove(req.body,function(doc){
+        res.redirect('/article/list/');
+    });
 };
 // get add
 Article.prototype.get_add = function(req, res, next){
-    res.send('Hello from Worker add');
+    var self = this;
+    object = { pagename: 'Article add', userName: 'DiegoUG', csrfToken: req.csrfToken()};
+    self.view.add(res,object);
 };
 // get see
 Article.prototype.get_see_data = function(req, res, next){
-    res.send('Hello from Worker article see');
+    var self = this;
+    object = { pagename: 'Article add', userName: 'DiegoUG',  csrfToken: req.csrfToken()};
+    this.model.get({slug:req.params.data},function(doc){
+        object.article = doc[0];
+        self.view.see(res,object);
+    });
 };
 // get edit
 Article.prototype.get_edit_data = function(req, res, next){
-    res.send('Hello from Worker edit');
+    var self = this;
+    object = { pagename: 'Article edit', userName: 'DiegoUG'};
+    this.model.get({slug:req.params.data},function(doc){
+        object.article = doc[0];
+        self.view.add(res,object);
+    });
 };
 // get list
 Article.prototype.get_list = function(req, res, next){
-    res.send('Hello from Worker list');
+    var self = this;
+    object = { pagename: 'Article list', userName: 'DiegoUG'};
+    this.model.get({},function(docs){
+        object.articles = docs;
+        self.view.list(res,object);
+    });
 };
 // export module
 module.exports = Article;
