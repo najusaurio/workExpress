e673d1fdb76f78ec312fbccaa67c30503f4b9c09
DiegoUG <diego.uribe.gamez@gmail.com>
2015-06-21 18:53:57 -0500

    [update] home controller

--- app/website/controllers/home.js
+++ app/website/controllers/home.js
@@ -18,42 +18,6 @@ Home.prototype.routes = function(){
         var object = { pagename: 'Home', userName: 'DiegoUG'};
         self.view.see(res,object);
     });
-    // post save
-    this.express.post('/home/save/',function(req,res){
-        self.model.get(function(docs){
-            if(docs.length === 0){
-                req.body.name = 'Home';
-                var data = {
-                    status: req.body.active,
-                    title: req.body.name,
-                    slug: req.body.name.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,''),
-                    content: req.body.content,
-                    description: req.body.description
-                };
-                self.articleModel.save(data,function(doc){
-                    var data = {
-                        active: req.body.active,
-                        name: req.body.name,
-                        $push: { item: {
-                            itemName: req.body.name.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,''),
-                            type: 'page',
-                            page: doc._id
-                        } },
-                        location: 'home'
-                    };
-                    self.model.save({name:'Home'},data,function(doc){
-                        res.redirect('/');
-                    });
-                });
-            }
-        });
-    });
-    // get edit
-    this.express.get('/home/edit/',function(req,res){
-        object = { pagename: 'Home' , userName: 'DiegoUG'};
-        self.view.edit(res,object);
-    });
-
 };
 // export module
 module.exports = Home;
