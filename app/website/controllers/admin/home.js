7e0835968bd6ab7d6f8a1ab115b7f31c2142dd68
DiegoUG <diego.uribe.gamez@gmail.com>
2015-06-21 18:52:43 -0500

    [add] baseurl string to admin home

--- app/website/controllers/admin/home.js
+++ app/website/controllers/admin/home.js
@@ -1,5 +1,6 @@
 // dependencies
-var HomeView = require('../../views/admin/home');
+var baseUrl  = '/admin/',
+    HomeView = require('../../views/admin/home');
 // module
 var Admin = function(conf){
     this.conf     = conf || {};
@@ -10,8 +11,8 @@ var Admin = function(conf){
 // routes
 Admin.prototype.routes = function(){
     var self = this;
-    // root
-    this.express.get('/admin/',function(req,res){
+    // root admin
+    this.express.get(baseUrl,function(req,res){
         self.view.admin(res,{});
     });
 };
