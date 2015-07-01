69907cca4a1c8a58b752d92d4eefbbc9d5fb438a
DiegoUG <diego.uribe.gamez@gmail.com>
2015-07-01 14:10:25 -0500

    [update] move user form data to user html

---
+++ app/website/views/home.js
@@ -0,0 +1,14 @@
+// module
+var Home = function(conf){
+    this.conf = conf || {};
+};
+// render see
+Home.prototype.see = function(res,object){
+    res.render('home_see',object);
+};
+// render edit
+Home.prototype.edit = function(res, object){
+    res.render('home_edit', object);
+};
+// export module
+module.exports = Home;
