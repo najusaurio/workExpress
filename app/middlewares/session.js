d588799e738cce025d7d0cc163ba2245dc337820
DiegoUG <diego.uribe.gamez@gmail.com>
2015-09-29 18:12:10 -0500

    [update] expand session middleware

--- app/middlewares/session.js
+++ app/middlewares/session.js
@@ -1,5 +1,17 @@
-var conf          = require('../../conf'),
-    session       = require('express-session'),
-    sessionStore  = new session.MemoryStore();
+// dependencies
+var UserModel = require('../website/models/user'),
+    ObjectId = require('mongoose').Types.ObjectId,
+    model = new UserModel();
 
-module.exports = session({ secret: conf.secret, store: sessionStore });
+// module
+module.exports = function (req, res, next) {
+    var uid = req.session.uid;
+    if (!uid) return next();
+    if (!req.user || !res.locals.user){
+        model.getById({_id:uid},function(user){ // falta colocarle a esta funcion err
+            if (!user) return next();
+            req.session.sessionUser = res.locals.sessionUser = user.toObject();
+            next();
+        });
+    }
+};
